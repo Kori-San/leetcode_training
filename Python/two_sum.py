4186adfe205476ca88a9467a03a249e46696cc5d
Kori <62435513+Kori-San@users.noreply.github.com>
2022-07-08 23:13:47 +0200

    Create two_sum.py

---
+++ Python/two_sum.py
@@ -0,0 +1,13 @@
+# https://leetcode.com/problems/two-sum/submissions/
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        for i in range(len(nums)):
+            seeked = target - nums[i]
+            if seeked in nums[i + 1::]:
+                nums[i] = "X"
+                return [i, nums.index(seeked)]
