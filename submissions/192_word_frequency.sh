afd6dbeab52a5f842baf7a570004b1f58a47b3e1
Kori <62435513+Kori-San@users.noreply.github.com>
2022-07-09 18:07:54 +0200

    Create 192_word_frequency.sh

---
+++ submissions/192_word_frequency.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Read from the file words.txt and output the word frequency list to stdout.
+cat words.txt | tr '\n' ' ' | tr -s ' ' | sed 's/ $/\n/g' | sed 's/ /\n/g' | sort | uniq -c | sort -r | sed 's/^ *//g' | sed 's/\([0-9]*\)\(.*\)/\2 \1/g' | cut -c2-
